3692164ab050e8a7423ce63d750d6d9c3b60b0b2
Gorantla <sgorantla@sgorantlamac1.(none)>
2011-11-05 11:54:21 -0400

    first commit

---
+++ src/main/java/com/eventestimator/mvc/EventController.java
@@ -0,0 +1,21 @@
+package com.eventestimator.mvc;
+
+/**
+ * Copyright 2008 by Constant Contact Inc.,
+ * Boston, MA 02451, USA
+ * Phone: (781) 472-8100
+ * Fax: (781) 472-8101
+ * Created on: 	 11/5/11
+ *
+ * @author sgorantla
+ *         All rights reserved.
+ *         <p/>
+ *         This software is the confidential and proprietary information
+ *         of Constant Contact, Inc. created for Constant Contact, Inc.
+ *         You shall not disclose such Confidential Information and shall use
+ *         it only in accordance with the terms of the license agreement
+ *         you entered into with Constant Contact, Inc.
+ */
+
+public class EventController {
+}
