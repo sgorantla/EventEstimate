dee7ca26b1c4a725982eab7cb1e27750e7c03ff4
David Greenfield <dgreenfield0@gmail.com>
2011-11-06 11:06:02 -0500

    added geoAddress to mapfunctions

--- src/main/webapp/js/mapfunctions.js
+++ src/main/webapp/js/mapfunctions.js
@@ -4,6 +4,24 @@
  */
 
 
+function GeoAddress(address) {
+		var geoLocale;
+	  	geocoder.geocode( { 'address': address}, function(results, status) {
+	    if (status == google.maps.GeocoderStatus.OK) {
+	      map.setCenter(results[0].geometry.location);
+	      var marker = new google.maps.Marker({
+	          map: map,
+	          position: results[0].geometry.location
+// 	          geoLocale.push(address,"","",);
+// 	      	  geoData.push(geoLocale);
+	     	});
+	    } else {
+	      alert("Geocode was not successful for the following reason: " + status);
+	    }
+	  });
+}
+
+
 function setContactMarkers(map, locations) {
 	  var image = new google.maps.MarkerImage('images/spot_contact.png');
 	  for (var i = 0; i < locations.length; i++) {
