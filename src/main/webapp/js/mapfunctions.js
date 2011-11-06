98937ea3cd55cb155d144bfc479ee66635dec90b
David Greenfield <dgreenfield0@gmail.com>
2011-11-06 11:10:27 -0500

    Updated geoSetAddress to set contacts icon

--- src/main/webapp/js/mapfunctions.js
+++ src/main/webapp/js/mapfunctions.js
@@ -6,11 +6,13 @@
 
 function GeoSetAddress(map, address) {
 		var geoLocale;
-	  	geocoder.geocode( { 'address': address}, function(results, status) {
+		var image = new google.maps.MarkerImage('images/spot_contact.png');
+		geocoder.geocode( { 'address': address}, function(results, status) {
 	    if (status == google.maps.GeocoderStatus.OK) {
 	      map.setCenter(results[0].geometry.location);
 	      var marker = new google.maps.Marker({
 	          map: map,
+	          icon: image,
 	          position: results[0].geometry.location
 // 	          geoLocale.push(address,"","",);
 // 	      	  geoData.push(geoLocale);
