e633956cf04665eed54175e2cbe35a29bfbf41a8
David Greenfield <dgreenfield0@gmail.com>
2011-11-06 11:07:14 -0500

    updated methodname

--- src/main/webapp/js/mapfunctions.js
+++ src/main/webapp/js/mapfunctions.js
@@ -4,7 +4,7 @@
  */
 
 
-function GeoAddress(map, address) {
+function GeoSetAddress(map, address) {
 		var geoLocale;
 	  	geocoder.geocode( { 'address': address}, function(results, status) {
 	    if (status == google.maps.GeocoderStatus.OK) {
